95de3e88123920f46ce5cd5d5763842b7d6bbcf1
Allquantor <nilssmol@gmail.com>
2020-03-26 18:13:35 +0100

    key added

--- burger-builder/src/components/OrderDetails/OrderDetails.js
+++ burger-builder/src/components/OrderDetails/OrderDetails.js
@@ -3,7 +3,7 @@ import React from 'react'
 const OrderDetails = (props) => {
 
     const data = Object.keys(props.ingredients).map(ing => {
-        return <div>{ing}: {props.ingredients[ing]}</div>
+        return <div key={ing}>{ing}: {props.ingredients[ing]}</div>
     })
 
     return data;
