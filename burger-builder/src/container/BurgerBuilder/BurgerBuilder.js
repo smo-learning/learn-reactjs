107d95bfede35ce7ce6c7ce64971067a8f8fa63b
Allquantor <nilssmol@gmail.com>
2020-03-26 18:12:05 +0100

    Modal + Backdrop component (copied) + Order the burger

--- burger-builder/src/container/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/container/BurgerBuilder/BurgerBuilder.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 
 import Burger from '../../components/Burger/Burger'
 import BurgerControls from '../../components/BurgerControls/BurgerControls'
-
+import Modal from '../../components/UI/Modal/Modal'
+import OrderDetails from '../../components/OrderDetails/OrderDetails'
 export default class BurgerBuilder extends Component {
 
     state = {
@@ -11,7 +12,8 @@ export default class BurgerBuilder extends Component {
             cheese: 2,
             meat: 0,
             bacon: 0
-        }
+        },
+        showModal: false
     }
 
     addIngredientHandler = (ingredient) => {
@@ -26,14 +28,28 @@ export default class BurgerBuilder extends Component {
         this.setState({ ingredients: curIngredients });
     }
 
+    orderHandler = () => {
+        this.setState({ showModal: true });
+    }
+
+    toggleModal = () => {
+        const curShow = this.state.showModal;
+        this.setState({ showModal: !curShow });
+    }
+
     render() {
         return (
             <div>
+                <Modal show={this.state.showModal} modalClosed={this.toggleModal}>
+                    <OrderDetails ingredients={this.state.ingredients}></OrderDetails>
+                </Modal>
                 <Burger ingredients={this.state.ingredients}></Burger>
                 <BurgerControls
                     ingredients={this.state.ingredients}
                     addIngredient={this.addIngredientHandler}
-                    removeIngredient={this.removeIngredientHandler}></BurgerControls>
+                    removeIngredient={this.removeIngredientHandler}
+                    purchasable={true}
+                    ordered={this.orderHandler}></BurgerControls>
             </div>
         )
     }
